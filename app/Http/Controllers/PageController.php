aa5ffeda9334e544192eec919c6951c5966432f3
StefanoMRC <stefano.mrc92@gmail.com>
2022-05-31 15:46:44 +0200

    passaggio dati alla home

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Book;
 
 class PageController extends Controller
 {
@@ -13,7 +14,9 @@ class PageController extends Controller
      */
     public function index()
     {
-        //
+        $books=Book::all();
+        
+        return view('home', compact('books'));
     }
 
     /**
